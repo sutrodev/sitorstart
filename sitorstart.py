aa9ffc3c8d80c3451d8eca1d3919fafeec835f1e
Jon Wynacht <jon@wynacht.org>
2013-04-16 06:52:09 -0700

    Signed-off-by: Jon Wynacht <jon@wynacht.org>
    Added basic Twitter search and requests lib.

--- sitorstart.py
+++ sitorstart.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 import csv
+import argparse 
+import requests
 
 from team import Team
 
 
 league = {}
+team = None
+BASE_TWITTER_SEARCH='http://search.twitter.com/search.json?q=%s&src=typd'
 
 
 def setup_league():
@@ -28,10 +32,26 @@ def print_league():
         print '\n'
 
 
-def main():
-    setup_league()
-    print_league()
+def init_team(team_name):
+    team = Team(team_name, league[team_name])
+    search_team(team)
+           
+
+def search_team(team):
+    for player in team.players:
+        url = BASE_TWITTER_SEARCH % player
+        r = requests.get(url)
+        player_results = r.json()
+        for results in player_results['results']:
+            print results['text']
+            print '\n\n'
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Let Twitter set your linup!')
+    parser.add_argument('-t', '--team', help='Your team name', required=True)
+    team_name = parser.parse_args().team
+
+    setup_league()
+    print_league()
+    init_team(team_name)
